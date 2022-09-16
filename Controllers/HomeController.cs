8e44b64b0573b90537d22c0c551b7706f6b28506
Gabriel Morais <gabe-morais@outlook.com>
2022-09-15 21:13:34 -0300

    Adicionando Detalhes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,9 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Pokedex.Data;
 using Pokedex.Models;
+using Pokedex.ViewModels;
 
 namespace Pokedex.Controllers;
 
@@ -27,9 +27,28 @@ public class HomeController : Controller
         return View(pokemons);
     }
 
-    public IActionResult Details(){
-        return View();
-        
+    public IActionResult Details(uint Number)
+    {
+        var current = _context.Pokemons
+            .Include(pokemon => pokemon.Types).ThenInclude(pt => pt.Type)
+            .Include(p => p.Gender).Include(p => p.Generation)
+            .Where(p => p.Number == Number).SingleOrDefault();
+
+        var prior = _context.Pokemons.OrderByDescending(p => p.Number)
+            .Where(p => p.Number < Number).FirstOrDefault();
+
+        var next = _context.Pokemons.OrderBy(p => p.Number)
+            .Where(p => p.Number > Number).FirstOrDefault();
+
+        var pokemon = new Details()
+        {
+            Prior = prior,
+            Current = current,
+            Next = next
+        };
+
+        return View(pokemon);
+
     }
 
     public IActionResult Privacy()
---
+++ ViewModels/Details.cs
@@ -0,0 +1,16 @@
+using System;
+using Pokedex.Models;
+
+namespace Pokedex.ViewModels
+{
+    public class Details
+    {
+        public Pokemons? Prior { get; set; }
+
+        public Pokemons? Current { get; set; }
+
+        public Pokemons? Next { get; set; }
+        
+
+    }
+}
